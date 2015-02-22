3ddddaae09b6c25e6e1d2aac187e8e0f001588ed
raicadaudinh <tranvi_2102@yahoo.com>
2015-02-22 23:32:23 +0100

    them

--- project/ui.R
+++ project/ui.R
@@ -17,6 +17,21 @@ shinyUI(fluidPage(
                        
                        
                 ),
-                mainPanel(plotOutput("chart"))
+                mainPanel(
+                        tabsetPanel(
+                                tabPanel("output", plotOutput("chart")),
+                                tabPanel("Documentation", p(p("Hello, in this document I present how to use my first shiny app!! "),
+                                                                    
+                                                                    p("This application show us the projection of European Union's and its members' demography from 2015-2060(80) (depend on case)."),
+                                                                    
+                                                                    p("You can find the data in data folder. The raw data has been downloaded from Eurostat web page"),
+                                                                    p("You can choose a country by the select bar."),
+                                                                    p("Then You can choose a scenario for the projection."), 
+                                                                    p("The projection changes from a scenario to another"),
+                                                                    p("There are 4 scenarii in total: main scenario, a scenario when there are no migration, a scenario when we have a higher age expectancy and a scenario with lower fertility rate.")
+                                )
+                        )
+                )
+                )
         ))
 )
